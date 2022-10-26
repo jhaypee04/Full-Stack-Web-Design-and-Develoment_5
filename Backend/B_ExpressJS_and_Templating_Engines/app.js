9475e5b9495bac741d297696e18c2b24b4c7ce94
Johnpaul <pauledozie004@gmail.com>
2022-10-26 11:37:37 +0100

    Learnt ExpressJS

--- Backend/B_ExpressJS_and_Templating_Engines/app.js
+++ Backend/B_ExpressJS_and_Templating_Engines/app.js
@@ -1,1 +1,15 @@
-console.log('working')
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to my new WebApp!</h1>')
+})
+
+app.get('/about', (req,res)=>{
+    res.send('<p>This is the about page!</p>')
+})
+
+app.listen(3000,()=>{
+    console.log('App is started/connected on port 3000')
+})
